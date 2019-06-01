d046a19ab66dda4a8380e4579d9b70aef7a4ba77
Pignataro67 <Pignataro67@gmail.com>
2019-06-01 10:46:54 -0700

    adding search and search input components in search folder

---
+++ client/src/components/search/Search.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import SearchInput from './SearchInput';
+import Button from '../Button';
+import Card from '../Card';
+
+class Search extends Component {
+
+  state = {
+    startingLocation: '',
+    destination: ''
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  handleChangeStart = (e) => {
+    this.setState({
+      startingLocation: e.target.value,
+    })
+  }
+
+  handleChangeDestination = (e) => {
+    this.setState({
+      destination: e.target.value
+    })
+  }
+
+  handleStartSearch = (e) => {
+    e.preventDefault()
+    e.stopPropogation()
+
+    this.props.actionsfetchStartingLocation(this.state.startingLocation)
+  }
+
+  handleDropOffSearch = (e) => {
+    console.log(this.state.dropOff)
+    e.preventDefault()
+    e.stopPropogation()
+    this.props.actions.fetchDropOff(this.state.dropOff)
+  }
+
+  handleUpdateStartAddress = (e) => {
+    this.setState({
+      startingLocation: e.currentTarget.innerText,
+    })
+  }
+
+  handleUpdateDropOffAddress = (e) => {
+    this.setState({
+      dropOff: e.currentTarget.innerText,
+    })
+  }
+
+  render() {
+    return (
+      <Card >
+          <SearchInput label="Pickup Location" 
+          
+          suggestedLocations = {this.props.suggestedStartingLocations} 
+          onChange={this.handleChangeStart} 
+          onSubmit={this.handleStartSearch}
+          handleUpdateAddress={this.handleUpdateAddress} 
+          value={this.state.pickupLocation}/>
+          <br/>
+          <SearchInput label="DropOff" 
+            suggestedLocations={this.props.suggestedDropOffs}
+            onChange={this.handleChangeDestination} 
+            onSubmit={this.handleDropOffSearch}
+            handleUpdateAddress={this.handleUpdateDropOffAddress}/>
+          <br/>
+          <Button buttonTitle="Submit" onClick={this.handleFormSubmit}/>
+      </Card >
+    )
+  }
+}
+
+export default Search;
---
+++ client/src/components/search/SearchInput.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Dropdown } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
+
+class SearchInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isDropDownOpen: false,
+      suggestedLocations: []
+    }
+  }
+  
+  componentDidUpdate(prevProps) {
+    if(prevProps.suggestedLocations !==
+    this.props.suggestedLocations) {
+      this.addSuggestedLocationsToState(this.props.suggestedLocations)
+    }
+  }
+
+  addSuggestedLocationsToState = (locations) => {
+    const suggestedLocations = locations.map(({place_id, id, description}) => ({key: id, value: place_id, text: description}))
+    this.setState({
+      suggestedLocations,
+      isDropDownOpen: true
+    })
+  }
+
+  handleDropDownChange = (e) => {
+    this.setState({
+      isDropDownOpen: false
+    })
+    this.props.handleUpdateAddress(e)
+  }
+
+  onFocus = () => this.setState({ isDropDownOpen: true },
+    () => this.setState({ isDropDownOpen: false }));
+    
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        <Dropdown 
+          onSearchChange={this.props.onChange}
+          onChange={this.handleDropDownChange} 
+          placeholder={this.props.label} 
+          search selection
+          onFocus={this.onFocus}
+          options={this.state.suggestedLocations} 
+          open={this.state.isDropDownOpen || false}
+          icon={<span></span>}
+      />
+      <Button Icon>
+        <Icon name='search'/>
+      </Button>
+    </form>
+    )
+  }
+}
+    
+export default SearchInput;

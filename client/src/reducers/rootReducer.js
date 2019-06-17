bb9adc08786d1d94be9b66e5a1d9883d5f51d8dc
Pignataro67 <Pignataro67@gmail.com>
2019-06-17 14:39:34 -0700

    updating rootReducer with combine Red. func.

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,19 +1,14 @@
-export default function rootReducer(state = {
-  isFetchingStartingLocation: false,
-  isFetchingDropOff: false,
-  suggestedPickupLocations: [],
-  suggestedDropOff: [],
-    }, action) {
-    switch(action.type){
-      case "FETCHING_SUGGESTED_PICKUP_LOCATIONS":
-        return {...state, isFetchingPickupLocation: true, suggestedPickupLocations: []}
-      case "DISPLAY_PICKUP_LOCATIONS":
-        return {...state, isFetchingPickupLocation: false, suggestedPickupLocations: action.suggestedPickupLocations}
-      case "FETCHING_SUGGESTED_DROPOFFS": 
-        return {...state, isFetchingDropOff: true, suggestedDropOffs: []}
-      case "DISPLAY_DROPOFFS":
-        return {...state, isFetchingDropOff: false, suggestedDropOffs: action.suggestedDropOffs}
-      default: 
-        return state
-    }
-  }
+import { combineReducers } from 'redux';
+import confirmRoutereducer from './confirmRouteReducer';
+import mapboxReducer from './mapboxReducer';
+import resultsReducer from './resultsReducer';
+import searchReducer from './searchReducer';
+
+const rootReducer = combineReducers({
+  mapboxReducer,
+  searchReducer,
+  confirmRoutereducer,
+  resultsReducer
+})
+
+export default rootReducer;
